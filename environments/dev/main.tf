4d1f9cfb285052fe37a164864750fe723357e75e
Snezana <SneX13@users.noreply.github.com>
2021-11-16 00:48:42 +0100

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -29,7 +29,7 @@ module "vpc" {
   
 module "vm_instance" {
   source  = "../../modules/vm_instance"
-  network = "${module.vpc.network_name}"
+  network = "${var.network}"
   num_instances = "${var.num_instances}"
 }
 module "http_server" {
